cfa5ccc5392ea38db1c7bf116739beede7d2b587
Jesus <jesuspl@tutanota.com>
2022-09-14 21:30:37 +0200

    Adding remove and add todo in memory

--- app/src/main/java/com/jesuspinar/todo/MainActivity.java
+++ app/src/main/java/com/jesuspinar/todo/MainActivity.java
@@ -1,12 +1,15 @@
 package com.jesuspinar.todo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -38,16 +41,34 @@ public class MainActivity extends AppCompatActivity {
                 addItem(view);
             }
         });
-
-
     }
-    // TODO: Implement action
     private void setUpListViewListener() {
-
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Creating toast message
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+                //Remove item
+                items.remove(i);
+                //Updating Listing
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 
-    // TODO: Implement action
     private void addItem(View view){
-
+        //Getting description input
+        EditText input = findViewById(R.id.descriptionTodo);
+        String itemText = input.getText().toString();
+        //Adding new to do
+        if(!(itemText.equals(""))){
+            itemsAdapter.add(itemText);
+            //Cleaning input
+            input.setText("");
+        }else{
+            Toast.makeText(getApplicationContext(),"Field is empty", Toast.LENGTH_LONG).show();
+        }
     }
 }
