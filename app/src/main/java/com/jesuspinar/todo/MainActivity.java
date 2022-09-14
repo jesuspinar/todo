d2a92371946ef713a79c5622a1659d268f471e40
Jesus <jesuspl@tutanota.com>
2022-09-14 19:58:00 +0200

    Preparing listeners structure

--- app/src/main/java/com/jesuspinar/todo/MainActivity.java
+++ app/src/main/java/com/jesuspinar/todo/MainActivity.java
@@ -2,7 +2,10 @@ package com.jesuspinar.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,10 +15,39 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
     private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Linking UI elements with variables
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(itemsAdapter);
+        setUpListViewListener();
+
+        //Event listener button pressed
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+
+    }
+    // TODO: Implement action
+    private void setUpListViewListener() {
+
+    }
+
+    // TODO: Implement action
+    private void addItem(View view){
+
     }
 }
