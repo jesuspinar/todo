c36f5067a9b148804d0d5b6d2a49f348e7e852bf
Jesus <jesuspl@tutanota.com>
2022-10-26 12:02:46 +0200

    Saving data to a Json file

--- app/src/main/java/com/jesuspinar/todo/MainActivity.java
+++ app/src/main/java/com/jesuspinar/todo/MainActivity.java
@@ -1,58 +1,76 @@
 package com.jesuspinar.todo;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.jesuspinar.todo.model.ToDo;
 
 import java.io.Serializable;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements Serializable {
 
     private ArrayList<ToDo> items;
     private ArrayAdapter<ToDo> itemsAdapter;
-    private ListView listView;
-    private Button btnAdd;
-    private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-        btnAdd = findViewById(R.id.btnAdd);
-        btnSave = findViewById(R.id.btnSave);
+        ListView listView = findViewById(R.id.listView);
+        Button btnAdd = findViewById(R.id.btnAdd);
 
-        items = new ArrayList<ToDo>();
+        loadData();
         itemsAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(itemsAdapter);
 
         //Event listeners
-        listViewListener();
-        btnAdd.setOnClickListener(this::addItem);
+        btnAdd.setOnClickListener(v -> addItem());
+        listView.setOnItemLongClickListener((p, v, i, id) -> removeItem(i));
     }
 
-    /**
-     * Sets a listener to remove a to do when long click, and updates list */
-    private void listViewListener() {
-        listView.setOnItemLongClickListener((adapterView, view, i, l) -> {
-            itemsAdapter.remove(items.get(i));
-            itemsAdapter.notifyDataSetChanged();
-            Toast.makeText(getApplicationContext(), getString(R.string.deleted_todo), Toast.LENGTH_SHORT).show();
-            return true;
-        });
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        saveData();
     }
 
-    private void addItem(View view){
+    private void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("todos",null);
+        Type type = new TypeToken<ArrayList<ToDo>>() {}.getType();
+        items = gson.fromJson(json,type);
+        if (items == null) items = new ArrayList<ToDo>();
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(items);
+        editor.putString("todos", json);
+        editor.apply();
+    }
+
+    private boolean removeItem(int i){
+        itemsAdapter.remove(items.get(i));
+        itemsAdapter.notifyDataSetChanged();
+        Toast.makeText(getApplicationContext(), getString(R.string.deleted_todo), Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    private void addItem(){
         EditText tvDescription = findViewById(R.id.tvDescription);
         String description = tvDescription.getText().toString();
         if(!(description.equals(""))){
